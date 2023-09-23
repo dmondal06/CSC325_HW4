5422c4dffea5274f73e0fbcbb67262c3d00e04d1
Deepa Mondal <143629428+dmondal06@users.noreply.github.com>
2023-09-22 23:05:15 -0400

    Update Staff.java

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -14,11 +14,19 @@ public class Staff
     {
         staffList = new StaffMember[6];
 
-        // TODO 1: Add 6 emploees to the staffList
+        // Initialize staff members
+        staffList[0] = new Executive("Alice", "123 Main St", "555-123-4567", "123-45-6789", 1000.0);
+        staffList[1] = new Intern("Nova Alim", "56 Scranton Lane", "333-222-1122", "555-555-5555", 0);
+        staffList[2] = new TempEmployee("Bob", "45 Pineapple Lane", "444-333-2222", "000-000-0012", 20);
+        staffList[3] = new TempEmployee("Deepa", "56 Night Lane", "545-555-9292", "345-768-2334",10);
+        staffList[4] = new Intern("Meow Chang", "89 Meow Lane", "444-222-6767", "999-545-7777", 0);
+        staffList[5] = new StaffEmployee("Bork Wolf", "31 Elm street", "631-345-9292", "435-768-5634",10);
 
-        ((Executive)staffList[0]).awardBonus(500.00);
+        // Award a bonus to the executive
+        ((Executive) staffList[0]).awardBonus(500.0);
 
-        ((TempEmployee)staffList[3]).addHours(40);
+        // Add hours worked for TempEmployee
+        ((TempEmployee) staffList[2]).addHours(40);
     }
 
     //-----------------------------------------------------------------
